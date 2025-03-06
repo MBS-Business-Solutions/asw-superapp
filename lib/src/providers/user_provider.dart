907b9cbdf7f8af274bfeb500addf773e8c8d0baf
Wanpaya <mumuu-xg@hotmail.com>
2025-03-06 10:46:25 +0700

    แก้บั๊กข้อมูล secure ไม่ลบเมื่อลบแอป แก้บั๊ก logout ไม่ลบ isar

--- lib/src/providers/user_provider.dart
+++ lib/src/providers/user_provider.dart
@@ -1,12 +1,18 @@
 import 'dart:convert';
 
+import 'package:AssetWise/main.dart';
 import 'package:AssetWise/src/models/aw_content_model.dart';
 import 'package:AssetWise/src/services/aw_user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:AssetWise/src/models/aw_notification_model.dart';
 
 class UserProvider with ChangeNotifier {
-  late final FlutterSecureStorage secureStorage = FlutterSecureStorage(aOptions: _getAndroidOptions());
+  late final FlutterSecureStorage secureStorage = FlutterSecureStorage(
+    aOptions: _getAndroidOptions(),
+    iOptions: _getIosOptions(),
+  );
   String? _token;
   String? get token => _token;
   bool get isAuthenticated => _token != null;
@@ -29,7 +35,10 @@ class UserProvider with ChangeNotifier {
     _token = token;
   }
 
-  AndroidOptions _getAndroidOptions() => const AndroidOptions(encryptedSharedPreferences: true);
+  AndroidOptions _getAndroidOptions() =>
+      const AndroidOptions(encryptedSharedPreferences: true);
+  IOSOptions _getIosOptions() => const IOSOptions(
+      accessibility: KeychainAccessibility.unlocked_this_device);
 
   Future<bool> login(String userId) async {
     final tokenResponse = await AwUserService().login(userId);
@@ -49,14 +58,25 @@ class UserProvider with ChangeNotifier {
     return false;
   }
 
-  Future<bool> loginNewUser({required String type, required String phone, required String email, required String firstName, required String lastName}) async {
-    final tokenResponse = await AwUserService().loginNewUser(type: type, phone: phone, email: email, firstName: firstName, lastName: lastName);
+  Future<bool> loginNewUser(
+      {required String type,
+      required String phone,
+      required String email,
+      required String firstName,
+      required String lastName}) async {
+    final tokenResponse = await AwUserService().loginNewUser(
+        type: type,
+        phone: phone,
+        email: email,
+        firstName: firstName,
+        lastName: lastName);
     if (tokenResponse != null) {
       _token = tokenResponse.token;
       _userInformation = tokenResponse.userInformation;
 
       await secureStorage.delete(key: 'LOGOUT');
-      await secureStorage.write(key: 'USER_INFO', value: _userInformation!.toJson());
+      await secureStorage.write(
+          key: 'USER_INFO', value: _userInformation!.toJson());
       await secureStorage.write(key: 'SESSION_TOKEN', value: _token);
       notifyListeners();
       return true;
@@ -77,6 +97,10 @@ class UserProvider with ChangeNotifier {
     _userId = null;
     _userInformation = null;
     await secureStorage.deleteAll();
+    await isar.writeTxn(() async {
+      await isar.clear();
+    });
+
     notifyListeners();
   }
 
@@ -91,6 +115,19 @@ class UserProvider with ChangeNotifier {
   }
 
   Future<void> _loadPreviousData() async {
+    final sharedPref = await SharedPreferences.getInstance();
+    final isFirstLoad = sharedPref.getBool('FIRST_LOAD') ?? true;
+    if (isFirstLoad) {
+      final secureStorageUnlock = FlutterSecureStorage(
+        aOptions: _getAndroidOptions(),
+        iOptions: const IOSOptions(
+            accessibility: KeychainAccessibility.unlocked_this_device),
+      );
+      await secureStorageUnlock.deleteAll();
+      await secureStorage.deleteAll();
+      await sharedPref.setBool('FIRST_LOAD', false);
+    }
+
     _token = await secureStorage.read(key: 'SESSION_TOKEN');
     _isPinSet = await secureStorage.read(key: 'PIN') != null;
     _userId = await secureStorage.read(key: 'USER_ID');
@@ -106,8 +143,10 @@ class UserProvider with ChangeNotifier {
     }
   }
 
-  Future<bool> submitConsents(String consentId, Map<String, bool> consents) async {
-    final response = await AwUserService.submitConsents(_token!, consentId, consents);
+  Future<bool> submitConsents(
+      String consentId, Map<String, bool> consents) async {
+    final response =
+        await AwUserService.submitConsents(_token!, consentId, consents);
     return response;
   }
 
@@ -142,19 +181,23 @@ class UserProvider with ChangeNotifier {
     final userInformation = await AwUserService.getUserInformation(_token!);
     if (userInformation != null) {
       _userInformation = userInformation;
-      await secureStorage.write(key: 'USER_INFO', value: _userInformation!.toJson());
+      await secureStorage.write(
+          key: 'USER_INFO', value: _userInformation!.toJson());
     }
     return _userInformation;
   }
 
   Future<List<PersonalConsent>> fetchPersonalConsents() async {
     if (token == null) return [];
-    return await AwUserService.fetchPersonalConsents(_token!, language: userInformation?.language);
+    return await AwUserService.fetchPersonalConsents(_token!,
+        language: userInformation?.language);
   }
 
-  Future<PersonalConsentDetail?> fetchPersonalConsentDetail(String consentId) async {
+  Future<PersonalConsentDetail?> fetchPersonalConsentDetail(
+      String consentId) async {
     if (token == null) return null;
-    return await AwUserService.fetchPersonalConsentDetail(_token!, consentId, language: userInformation?.language);
+    return await AwUserService.fetchPersonalConsentDetail(_token!, consentId,
+        language: userInformation?.language);
   }
 
   Future<bool> submitConsent(String consentId, bool value) async {
@@ -164,11 +207,13 @@ class UserProvider with ChangeNotifier {
 
   Future<List<AboutItem>> fetchAboutItems() async {
     if (token == null) return [];
-    return await AwUserService.fetchAboutItems(_token!, language: userInformation?.language);
+    return await AwUserService.fetchAboutItems(_token!,
+        language: userInformation?.language);
   }
 
   Future<AboutItemDetail?> fetchAboutItemDetail(String consentId) async {
     if (token == null) return null;
-    return await AwUserService.fetchAboutItemDetail(_token!, consentId, language: userInformation?.language);
+    return await AwUserService.fetchAboutItemDetail(_token!, consentId,
+        language: userInformation?.language);
   }
 }
