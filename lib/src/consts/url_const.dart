98de3ff0f38fac8ac2873207c8da79cf1d1ae89d
wanpaya <mumuu-xg@hotmail.com>
2025-03-31 19:20:53 +0700

    Prod 1.0

--- lib/src/consts/url_const.dart
+++ lib/src/consts/url_const.dart
@@ -1,5 +1,8 @@
-// AWS
-const BASE_URL = 'https://dev-superapp-api.assetwise.co.th';
+// AWS - Development
+// const BASE_URL = 'https://dev-superapp-api.assetwise.co.th';
+
+// AWS - Production
+const BASE_URL = 'https://superapp-api.assetwise.co.th';
 
 // Azure
 // const BASE_URL = 'https://superapp-api.azurewebsites.net';
